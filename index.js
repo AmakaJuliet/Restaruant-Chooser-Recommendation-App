e19462bd612f1b5b959b1af9e814ab03a5fa69cd
Amaka Ibenegbu <113144586+AmakaJuliet@users.noreply.github.com>
2022-10-29 00:05:42 +0000

    Fixed an error

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ rl.question(`What is your name?`, function(name){rl.question(`How old are you?`,
   if(age < 15){
   console.log(`Hello ${name} we do not have the kids and teens options please check back later`); 
   rl.close();
-    process.exit(0);
+   process.exit(0);
 }
                                                      
   rl.question(`How much cash do you have?`, function(cash){cash = parseInt(cash);
